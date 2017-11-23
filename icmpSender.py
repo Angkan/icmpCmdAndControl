505d2c12fa19efe78c296f7e71ab2144d25072f4
nightcrawler <chanda.angkan@gmail.com>
2017-11-23 14:59:37 +0300

    Update icmpSender.py
    Second revision... much less noisy and works well for me atleast :)

--- icmpSender.py
+++ icmpSender.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python
 
 #importing modules
-from scapy.all import *
-import threading
-import time
-
-#this code is still in debug mode and has lots of print statements for debugging
-#modify as required but this is definately of very novice level
+try:
+	from scapy.all import *
+except Exception,e:
+	print "Exception: " + str(e)
 
 def send(ip_target):
 	while 1:
-		print "[+]Entering a loop..."
 		icmp = ICMP()
 		ip = IP()
 		icmp.type = 8
@@ -19,44 +16,34 @@ def send(ip_target):
 		data = "nytcrwlr"
 		data = data + raw_input("#")
 		data = data.strip("\r").strip("\n").lower()
-		print data
 		if data == "nytcrwlrexit":
-			packt = sr1(ip/icmp/data)
+			packt = sr1(ip/icmp/data,verbose=False)
 			sys.exit(0)
 		else:
-			print "[+]sending data"
-			packt = sr1(ip/icmp/data,timeout=1)
-			print "[+]sent!"
+			packt = sr1(ip/icmp/data,timeout=1,verbose=False)
 			output(ip_target)
 
 def output(ip_target):
 	flag = 1
-	print "[+]waiting for response"
 	while flag:
-		print "[+]waiting"
 		resp = sniff(iface="eth0", timeout=2)
 		for packet in resp:
-			print "Loop[1]"
 			if packet.haslayer(ICMP):
-				print "Loop[2]"
 				if str(packet.getlayer(ICMP).type) == "0":
 					flag = 0
-					print "[+]type code 0 got!"
 					out = packet.getlayer(Raw).load[0:]
-					print "data: " + str(out)
-					print "send packet to end server sr1"
+					print str(out)
 					icmp = ICMP()
 					ip = IP()
 					icmp.type = 8
 					icmp.code = 0
 					ip.dst = ip_target
-					sr1(ip/icmp,timeout=1)
-					print "[+]done"
+					sr1(ip/icmp,timeout=1,verbose=False)
 					send(ip_target)
 
 def main():
-	print "[+]starting..."
 	ip_target = raw_input("[+]Enter the Target IP address: ")
+	subprocess.call('clear',shell=True)
 	send(ip_target)
 
 if __name__ == "__main__":
